70e9053733fbb1b05d21f5dc48156ba5dacca835
Rett Lam <rettlam@gmail.com>
2020-06-05 20:44:23 -0600

    json deli

--- princess-in-other-castle/app.js
+++ princess-in-other-castle/app.js
@@ -1,48 +1,112 @@
 class Player {
-    constructor(name, type, status, hasStar, gameActive) {
+    constructor(name, status, hasStar, gameActive) {
         // { name = "name", type = 0, status = ("Powered Up", "Big", "Small", "Dead"), hasStar = false, setName = ("Mario", "Luigi"), gotHit, gotPowerup, gameActive = true }  ????
         this.name = name
-        this.type = type
-        this.status = status
-        this.hasStar = hasStar
-        this.gameActive = gameActive
+        this.status = "Small"
+        this.hasStar = false
+        this.gameActive = true
+        this.coinCount = 0
     }
 
-    // setName ()= ("Mario", "Luigi")
-    // addCoin()
-    // print()
-    // gotHit()
-    // gotPowerup()
+
+    setName() {
+        this.name = "Luigi"
+    }
+
+    addCoin() {
+        this.coinCount++
+    }
+
+    gotHit() {
+        if (this.hasStar === true) {
+            console.log(`The star has protected you!`)
+        } else if (this.status === "Powered Up") {
+            this.status = "Big"
+        } else if (this.status === "Big") {
+            this.status = "Small"
+        } else if (this.status === "Small") {
+            this.status = "Dead"
+            this.gameActive = false
+            console.log(`Good game, you died though!  Try again!`)
+        }
+    }
+
+    gotPowerup() {
+        if (this.status === "Small") {
+            this.status = "Big"
+        } else if (this.status === "Big") {
+            this.status = "Powered Up"
+        } else if (this.status === "Powered Up") {
+            this.hasStar = true
+        }
+    }
+
+    print() {
+        console.log(`Name: ${this.name}\nStatus: ${this.status}\nTotal Coins: ${this.coinCount}\n`)
+    }
+
+
 }
 
+
+
 const test = new Player()
+
+
 
 console.log(test)
 
 
-getRandomArbitrary = (min, max) => {
-    return Math.random() * (max - min) + min;
+const getRandomArbitrary = (max) => {
+    return Math.floor(Math.random() * Math.floor(max))
 }
 
-const coinCount = 0
 
-game = () =>{
-    let roll = getRandomArbitrary(0,2)
+// {
+//     return Math.floor(Math.random() * (max - min) + min);  // math.floor rounds to whole number
+// }
+
+
+
+let intervalID
+
+const turn = (Player) => {
+    if (!Player.gameActive) {
+        clearInterval(intervalID)
+    }
+    let roll = getRandomArbitrary(3)
     if (roll === 0) {
-        gotHit()
-    } else if (roll === 1){
-        gotPowerup()
-    } else if (roll === 2){
-        addCoin()
+        console.log(`Ouch you got hit!`)
+        Player.gotHit()
+        Player.print()
+    } else if (roll === 1) {
+        console.log(`You got a powerup!`)
+        Player.gotPowerup()
+        Player.print()
+    } else if (roll === 2) {
+        console.log(`Ding! A coin!`)
+        Player.addCoin()
+        Player.print()
     }
 }
 
 
-while (Player.status != "Dead"){
-    game()
+
+const game = (Player) => {
+    intervalID = setInterval(() => turn(Player), 1500) //remember no parantheses on function similar to addEventListener
 }
 
-console.log(`Good game, you died though!  Try again!`)
+// btn.addEventListener("click", () =>  myFunction(data))
+
+// function myFunction(data){
+
+// }    SIMILAR SYNTAX
+
+game(test)
+
+
+
+
 
 // Create a random range function that returns either 0, 1, or 2.
 
