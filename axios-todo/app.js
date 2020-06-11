7de455597e1af51d4991819eefd4b01c15b934b2
Rett Lam <rettlam@gmail.com>
2020-06-11 01:38:41 -0600

    axios todo complete

--- axios-todo/app.js
+++ axios-todo/app.js
@@ -9,16 +9,23 @@ displayList = (data) => {
     console.log(data) //TESTING AND UNECESSARY
     document.getElementById("list").textContent = ""
     for (let i = 0; i < data.length; i++) {
+        // let styleInsert = ""
         const li = document.createElement('li')
         const box = document.createElement("p")
-        li.innerHTML = `<input type="checkbox" name="button${i}"> ` + data[i].title + ` <button id="delete${i}" onclick="deleteItem(${i})">X</button>`
+        li.innerHTML = `<input type="checkbox" id="button${i}" onclick="completeToggle(${i})"> ` + data[i].title + ` <button id="delete${i}" onclick="deleteItem(${i})">X</button>`
         li.setAttribute("id", `line${i}`)
+        if (data[i].completed === true) {
+            li.setAttribute("style", "text-decoration: line-through")
+        }
         document.getElementById("list").appendChild(li)
         document.getElementById("list").appendChild(box)
         if (data[i].imgUrl) {
             const p = document.createElement('p')
             p.innerHTML = `<img src=${data[i].imgUrl} class="pics">`
             document.getElementById("list").appendChild(p)
+        }
+        if (data[i].completed === true) {
+            document.getElementById(`button${i}`).checked = true
         }
     }
 }
@@ -34,6 +41,27 @@ form.addEventListener("submit", (e) => {
 
 })
 
+
+
+const completeToggle = (i) => {
+    let id = 0
+    axios.get("https://api.vschool.io/rett/todo")
+        .then(results => completeToggle2(results.data[i]._id, i))
+        .catch(error => console.log(error))
+}
+
+
+const completeToggle2 = (id, i) => {
+    if (document.getElementById(`button${i}`).checked === true) {
+        axios.put("https://api.vschool.io/rett/todo/" + id, { completed: true })
+            .then(results => newPage())
+            .catch(error => console.log(error))
+    } else {
+        axios.put("https://api.vschool.io/rett/todo/" + id, { completed: false })
+            .then(results => newPage())
+            .catch(error => console.log(error))
+    }
+}
 
 
 const deleteItem = (i) => {
