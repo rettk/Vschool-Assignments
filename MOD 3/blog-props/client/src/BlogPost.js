23168426fe51ccb8925415e8f73510966eb71e1e
Rett Lam <rettlam@gmail.com>
2020-06-24 18:23:05 -0600

    standard React Install

--- MOD 3/blog-props/client/src/BlogPost.js
+++ MOD 3/blog-props/client/src/BlogPost.js
@@ -3,7 +3,7 @@ import React from "react"
 const BlogPost = (props) => {
     return (
         <div>
-            <h1>{props.title}</h1>
+            <h1 className="title">{props.title}</h1>
             <h2>{props.subTitle}</h2>
             <h3>Posted by {props.author} on {props.date}</h3>
         </div>
