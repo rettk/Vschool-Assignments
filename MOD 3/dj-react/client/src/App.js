a881e2ec6f777e18db97fdae3aede89b8f97e435
Rett Lam <rettlam@gmail.com>
2020-06-30 18:21:33 -0600

    dj states

--- MOD 3/dj-react/client/src/App.js
+++ MOD 3/dj-react/client/src/App.js
@@ -1,34 +1,53 @@
 import React from "react"
 
+// lights.map(light => <Light color={light}/>)
 
 class App extends React.Component {
 
     constructor() {
         super()
-        this.state = ["white", "white", "white", "white"]
+        this.state = {
+            colors: ["white", "white", "white", "white"]
+        }
     }
-
 
     render() {
         return (
-            <div id="djBox">
-                <div>
-                    <button>Press Me</button>
+            <div>
+                <div id="djBox">
+                    <div class="light" style={{ backgroundColor: this.state.colors[0] }}>
+
+                    </div>
+                    <div class="light" style={{ backgroundColor: this.state.colors[1] }}>
+
+                    </div>
+                    <div class="light" style={{ backgroundColor: this.state.colors[2] }}>
+
+                    </div>
+                    <div class="light" style={{ backgroundColor: this.state.colors[3] }}>
+
+                    </div>
                 </div>
-                <div>
-                    <button>Press Me</button>
-                </div>
-                <div>
-                    <button>Press Me</button>
-                </div>
-                <div>
-                    <button>Press Me</button>
+                <div id="buttonBox">
+                    <div class="button">
+                        <button onClick={this.state.colors[0] === "white" ?
+                            setState({ colors: ["black", "black", "black", "black"] }) :
+                            setState({ colors: ["white", "white", "white", "white"] })
+                        }>Black/White</button>
+                    </div>
+                    <div class="button">
+                        <button>Press Me</button>
+                    </div>
+                    <div class="button">
+                        <button>Press Me</button>
+                    </div>
+                    <div class="button">
+                        <button>Press Me</button>
+                    </div>
                 </div>
             </div>
-
         )
     }
-
 
 }
 
